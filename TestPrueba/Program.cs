59541de35931f5813fabd779c34ea3640f9e59cb
avdavilag <andersonypk@gmail.com>
2025-10-03 20:07:40 -0500

    Acabado el swirch subir

--- TestPrueba/Program.cs
+++ TestPrueba/Program.cs
@@ -5,78 +5,102 @@ Console.WriteLine(new string('*',100));
 
 var repositorioProducto = new RepositorioProductos();
 
-while (repositorioProducto.Count() < 10)
+while (!string.IsNullOrWhiteSpace("opcion"))
 {
-	Console.WriteLine("Ingrese el valor para el producto");
-    Console.WriteLine("Nombre del producto");
-    var producto = new Productos
+
+	Console.WriteLine("Seleccion una opcion");
+	Console.WriteLine("1. Agrega un producto");
+	Console.WriteLine("2. Editar un producto");
+	Console.WriteLine("3. Eliminar un producto");
+	Console.WriteLine("4. Mostrar un producto");
+    Console.WriteLine("5. Mostrar lista de Productos");
+    Console.WriteLine("6. Salir");
+	Console.WriteLine("----------------------");
+	var opcion = Console.ReadLine();
+
+	switch (opcion)
 	{
-		Nombre = Console.ReadLine()!
-	};
 
+		case "1":
+			var producto = new Productos
+			{
+				Nombre = Console.ReadLine()!
+			};
+			Console.WriteLine("Precio Producto");
+			if (double.TryParse(Console.ReadLine(), out double precio))
+			{
+				producto.Precio = precio;
+			}
+			else
+				producto.Precio = 100;
+			Console.WriteLine("Descripcion para el producto");
+			producto.Descripcion = Console.ReadLine() ?? string.Empty;
+			Console.WriteLine("Se agrego el producto {Enviroment.NewLine}");
+			repositorioProducto.Agregar(producto);
+			repositorioProducto.Mostrar_Productos();
+			break;
 
-	Console.WriteLine("Precio Producto");
-    if (double.TryParse(Console.ReadLine(), out double precio))
-	{
-		producto.Precio = precio;
-	}
-	else
-    producto.Precio = 100;
+		case "2":
+			Console.WriteLine("Edicion de un producto");
+			Console.WriteLine("Ingrese el codigo a editar");
+			var codigo = Console.ReadLine() ?? string.Empty;
+			var productoEncontrador = repositorioProducto.BuscarxCodigo(codigo);
+			if (productoEncontrador is not null)
+			{
+				Console.WriteLine("Nuevo nombre: ");
+				productoEncontrador.Nombre = Console.ReadLine()!;
+				Console.WriteLine("Nuevo precio: ");
+				productoEncontrador.Precio = double.Parse(Console.ReadLine()!);
+				Console.WriteLine("Descripcion Nueva");
+				productoEncontrador.Descripcion = Console.ReadLine();
+				Console.WriteLine("Dimensiones: (largo,ancho,alto)");
+				double largo, ancho, alto;
+				if (double.TryParse(Console.ReadLine(), out largo) &&
+					double.TryParse(Console.ReadLine(), out ancho) &&
+					double.TryParse(Console.ReadLine(), out alto))
+				{
+					productoEncontrador.Dimensiones = new Dimensiones(largo, ancho, alto);
+				}
+				Console.WriteLine("Producto Digitar (s/[n]: ");
+				var respuesta = Console.ReadLine();
+				productoEncontrador.TipoMercaderia = respuesta?.ToLower() == "s" ? TipoMercaderia.ProductoDigital : TipoMercaderia.ProductoFisico;
 
-	Console.WriteLine("Descripcion para el producto");
-	producto.Descripcion = Console.ReadLine() ?? string.Empty;
+				Console.WriteLine("Tipo de producto (1 - Producto / 2 - Servicio): ");
+				if (int.TryParse(Console.ReadLine(), out var tipoProducto))
+				{
+					productoEncontrador.TipoProducto = (TipoProducto)tipoProducto;
+				}
 
-    Console.WriteLine("Se agrego el producto {Enviroment.NewLine}");
-	repositorioProducto.Agregar(producto);
+				repositorioProducto.Editar(codigo, productoEncontrador);
+			}
+			else
+			{
+				Console.WriteLine("No se encontro el producto");
+			}
+			repositorioProducto.Mostrar_Productos();
+			break;
+		case "3":
+			Console.WriteLine("Eliminar un producto");
+			Console.WriteLine("Ingrese el codigo a eliminar");
+			var codigoEliminar = Console.ReadLine() ?? string.Empty;
+			repositorioProducto.Eliminar(codigoEliminar);
+			Console.WriteLine("Producto eliminado Correctamente");
+			 break;
+		case "4":
+			Console.WriteLine("Mostrar un producto");
+			Console.WriteLine("Ingresa el codigo de producto a consultar");
+			var codigoConsulta= Console.ReadLine() ?? string.Empty;
+			var productoConsulta = repositorioProducto.BuscarxCodigo(codigoConsulta);
+			Console.WriteLine("Producto que estas consultando es: "+productoConsulta.Descripcion);
+            break;
+		case "5":
+			Console.WriteLine("Muestra la lsita de productos");
+			repositorioProducto.Mostrar_Productos();
+			Console.WriteLine("--------------------------------");
+			break;
+			default:
+			Console.WriteLine("Gracias por su visita...");
+            break;
     }
 
-repositorioProducto.Mostrar_Productos();
-Console.WriteLine("Edicion de un producto");
-Console.WriteLine("Ingrese el codigo a editar");
-var codigo=Console.ReadLine() ?? string.Empty;
-
-//Console.WriteLine("Ingrese el nuevo nombre");
-//almacen.Editar(codigo, Console.ReadLine() ?? string.Empty);
-
-//Console.WriteLine("Ingrese el nuevo precio: ");
-//if(double.TryParse(Console.ReadLine(), out double precioEdit))
-//{
-//	almacen.Editar(codigo, precioEdit);
-//}
-
-var productoEncontrador=repositorioProducto.BuscarxCodigo(codigo);
-if (productoEncontrador is not null)
-{
-	Console.WriteLine("Nuevo nombre: ");
-	productoEncontrador.Nombre = Console.ReadLine()!;
-	Console.WriteLine("Nuevo precio: ");
-	productoEncontrador.Precio = double.Parse(Console.ReadLine()!);
-	Console.WriteLine("Descripcion Nueva");
-	productoEncontrador.Descripcion = Console.ReadLine();
-    Console.WriteLine("Dimensiones: (largo,ancho,alto)");
-	double largo, ancho, alto;
-	if (double.TryParse(Console.ReadLine(), out largo) &&
-		double.TryParse(Console.ReadLine(), out ancho) &&
-		double.TryParse(Console.ReadLine(), out alto))
-	{
-		productoEncontrador.Dimensiones = new Dimensiones(largo, ancho, alto);
-	}
-	Console.WriteLine("Producto Digitar (s/[n]: ");
-	var respuesta = Console.ReadLine();	
-	productoEncontrador.TipoMercaderia = respuesta?.ToLower() == "s" ? TipoMercaderia.ProductoDigital : TipoMercaderia.ProductoFisico;
-	
-	Console.WriteLine("Tipo de producto (1 - Producto / 2 - Servicio): ");
-	if(int.TryParse(Console.ReadLine(), out var tipoProducto))
-	{
-		productoEncontrador.TipoProducto = (TipoProducto)tipoProducto;
-    }
-
-    repositorioProducto.Editar(codigo,productoEncontrador);
 }
-else
-{
-	Console.WriteLine("No se encontro el producto");
-}
-repositorioProducto.Mostrar_Productos();
-
-
